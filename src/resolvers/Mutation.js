7bc1fe5521e2d1b453f718d19f3dc75b264a7747
riza.satyabudhi <riza.satyabudhi@aux.go-jek.com>
2018-12-31 00:11:40 +0700

    Add first Mutation & Query

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,18 @@
-const mutations = {};
+// check generated/prisma.graphql, find Mutations,
+// to see which function is available to be used in "ctx.db.mutation" or "ctx.db.query"
+const mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
+
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+        // title: args.title (other alternative)
+        // description: args.description (other alternative)
+      }
+    }, info)
+    return item
+  }
+};
 
 module.exports = mutations;
