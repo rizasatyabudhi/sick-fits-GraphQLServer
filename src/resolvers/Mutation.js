2c303c70f24e7477597c37068fd0985627d66441
riza.satyabudhi <riza.satyabudhi@aux.go-jek.com>
2018-12-31 17:54:56 +0700

    Update item Mutation & single item Query

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -12,6 +12,18 @@ const mutations = {
       }
     }, info)
     return item
+  },
+
+  updateItem(parent, args, ctx, info) {
+    const updates = { ...args }
+    delete updates.id
+    return ctx.db.mutation.updateItem({
+      data: updates,
+      where: {
+        id: args.id
+      }
+    },info
+    )
   }
 };
 
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,8 +1,11 @@
+const { forwardTo } = require('prisma-binding')
+
 const Query = {
   async items(parent, args, ctx, info) {
     const items = await ctx.db.query.items()
     return items
-  }
+  },
+  item: forwardTo('db') // alternative if the function has no modification from prisma.graphql
 };
 
 module.exports = Query;
